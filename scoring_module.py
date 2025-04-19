2ddc63ee5a088c0c614b156dbf1ed94beb2cccfb
jlizhiyi <66229513+jlizhiyi@users.noreply.github.com>
2025-04-19 12:32:45 -0400

    Update scoring_module.py

---
+++ scoring_module.py
@@ -0,0 +1,12 @@
+"""
+1 point - Each Ring Scored on a Stake
+3 points - Each Top Ring on a Stake
+3 points - Climb Level 1 (may be doubled if not contacting the Ladder)
+6 points - Climb Level 2 (may be doubled if not contacting the Ladder)
+12 points - Climb Level 3 (may be doubled if not contacting the Ladder)
+Each Ring Scored on a Mobile Goal that has been Placed in a Corner - See the Game Manual!
+High Stake Bonus - Additional 2 points per Climb for the Alliance with a Ring Scored on the High Stake
+"""
+
+# Simplify model so that each ring is worth 1 point?
+# Positive and negative corners
